ce24cd1012b0d88f1a4c51aa21056f944c68e349
xecarlox <jf94.uk@gmail.com>
2020-09-29 18:27:10 +0100

    data trasnfer node 3 to node 4

--- lab1/lab1.tcl
+++ lab1/lab1.tcl
@@ -32,15 +32,16 @@ set n3 [$ns node]
 set n4 [$ns node]
 
 # Create links between the nodes, bandwidth, speed and type of buffer
-$ns duplex-link $n0 $n2 2Mb 10ms DropTail
+$ns duplex-link $n0 $n2 2Mb 10ms RED
 $ns duplex-link $n1 $n2 2Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1.5Mb 10ms RED
-$ns duplex-link $n0 $n4 2Mb 10ms RED
+$ns duplex-link $n2 $n3 2Mb 10ms RED
+$ns duplex-link $n0 $n4 2Mb 10ms DropTail
 $ns duplex-link $n1 $n4 2Mb 10ms DropTail
 
 
 # setting queue size between link (n2-n3) to 10
-$ns queue-limit $n2 $n3 5
+$ns queue-limit $n2 $n3 7
+$ns queue-limit $n3 $n2 7
 
 
 # node position (For NAM)
@@ -59,13 +60,13 @@ set tcp [new Agent/TCP]
 $tcp set class_ 2
 
 # set node as a TCP sender
-$ns attach-agent $n0 $tcp
+$ns attach-agent $n3 $tcp
 
 # set TCP receiver agent
 set sink [new Agent/TCPSink]
 
 # set node as TCP receiver
-$ns attach-agent $n3 $sink
+$ns attach-agent $n4 $sink
 $ns connect $tcp $sink
 $tcp set fid_ 1
 
@@ -80,12 +81,12 @@ $ftp set type_ FTP
 set udp [new Agent/UDP]
 
 # set node as UDP sender
-$ns attach-agent $n1 $udp
+$ns attach-agent $n3 $udp
 
 
 # set UDP receiver agent
 set null [new Agent/Null]
-$ns attach-agent $n3 $null
+$ns attach-agent $n4 $null
 $ns connect $udp $null
 $udp set fid_ 2
 
@@ -93,20 +94,20 @@ $udp set fid_ 2
 set cbr [new Application/Traffic/CBR]
 $cbr attach-agent $udp
 $cbr set type_ CBR
-$cbr set packet_size_ 3000
+$cbr set packet_size_ 1000
 $cbr set rate_ 1mb
-$cbr set random_ true
+$cbr set random_ false
 
 
 # schedule events for CBR and FTP agents
 $ns at 0.0 "$cbr start"
-$ns at 0.1 "$ftp start"
+$ns at 0.3 "$ftp start"
 $ns at 1.0 "$ftp stop"
 $ns at 1.0 "$cbr stop"
 
 
 # detach tcp and sink agents
-$ns at 1.0 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n3 $sink"
+$ns at 1.0 "$ns detach-agent $n3 $tcp ; $ns detach-agent $n4 $sink"
 
 # finish procedure 
 $ns at 1.1 "finish"
