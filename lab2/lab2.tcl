905b7009ade489d3e358c0b35d589c6ecf9330c5
xecarlox94 <jf94.uk@gmail.com>
2020-10-05 16:27:05 +0100

    adding news nodes to lab 2

--- lab2/lab2.tcl
+++ lab2/lab2.tcl
@@ -28,11 +28,11 @@ set n3 [$ns node]
 set n4 [$ns node]
 
 
-$ns duplex-link $n0 $n2 1Mb 5ms DropTail
-$ns duplex-link $n4 $n0 1Mb 5ms DropTail
-$ns duplex-link $n1 $n4 1Mb 5ms DropTail
-$ns duplex-link $n2 $n1 1Mb 5ms DropTail
-$ns duplex-link $n3 $n2 1Mb 5ms DropTail
+$ns duplex-link $n0 $n2 1Mb 15ms DropTail
+$ns duplex-link $n4 $n0 1Mb 15ms DropTail
+$ns duplex-link $n1 $n4 1Mb 15ms DropTail
+$ns duplex-link $n2 $n1 1Mb 15ms DropTail
+$ns duplex-link $n3 $n2 1Mb 15ms DropTail
 
 
 $ns duplex-link-op $n0 $n2 orient right-down
@@ -43,6 +43,52 @@ $ns duplex-link-op $n3 $n2 orient left
 
 
 
-$ns at 1.0 "finish"
+
+
+
+
+set tcp1 [new Agent/TCP]
+$tcp1 set class_ 2
+$ns attach-agent $n0 $tcp1
+
+set sink1 [new Agent/TCPSink]
+$ns attach-agent $n3 $sink1
+$ns connect $tcp1 $sink1
+$tcp1 set fid_ 1
+
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+$ftp1 set type_ FTP
+
+
+
+
+
+set udp1 [new Agent/UDP]
+$ns attach-agent $n3 $udp1
+
+set null1 [new Agent/Null]
+$ns attach-agent $n4 $null1
+$ns connect $udp1 $null1
+$udp1 set fid_ 2
+
+set cbr1 [new Application/Traffic/CBR]
+$cbr1 attach-agent $udp1
+$cbr1 set type_ CBR
+$cbr1 set packet_size_ 1000
+$cbr1 set rate_ 1mb
+$cbr1 set random_ false
+
+
+
+
+
+$ns at 0.0 "$ftp1 start"
+$ns at 1.0 "$ftp1 stop"
+$ns at 0.1 "$cbr1 start"
+$ns at 0.7 "$cbr1 stop"
+
+
+$ns at 1.5 "finish"
 
 $ns run
