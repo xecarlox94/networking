00906839db7e1f2d37cbf21e6f5e4e8d9ffce481
xecarlox94 <jf94.uk@gmail.com>
2020-11-06 13:40:48 +0000

    working on the first topology

--- CW/Q2/top1.tcl
+++ CW/Q2/top1.tcl
@@ -21,8 +21,48 @@ proc finish {} {
 }
 
 
+for {set i 0} {$i < 7} {incr i} {
+    set n($i) [$ns node]
+}
 
 
+for {set i 0} {$i < 7} {incr i} {
+    $ns duplex-link $n($i) $n([expr ($i+1)%7]) 10Mb 10ms DropTail
+}
+
+
+proc set_udpCBR \{args} \{
+
+    # seeting UDP communication
+    set udp [new Agent/UDP]
+
+    $ns attach-agent $n(0) $udp
+
+    set null [new Agent/Null]
+    $ns attach-agent $n(3) $null
+    $ns connect $udp $null
+    $udp set fid_ 1
+
+
+    set cbr [new Application/Traffic/CBR]
+    $cbr attach-agent $udp
+    $cbr set type_ CBR
+    $cbr set packet_size_ 1000
+    $cbr set rate_ 1mb
+    $cbr set random_ false
+
+
+    $ns at 0.5 "$cbr start"
+    $ns at 4.5 "$cbr stop"
+
+}
+
+
+$ns rtmodel-at 1.0 down $n(1) $n(2)
+$ns rtmodel-at 2.0 up $n(1) $n(2)
+
+
+$ns rtproto LS
 
 
 
