ff6a9a9ef3c2b54eb2bb37813dd83e55c585579d
xecarlox94 <jf94.uk@gmail.com>
2020-11-11 20:57:52 +0000

    working on topology 2

--- CW/Q2/top1.tcl
+++ CW/Q2/top1.tcl
@@ -45,42 +45,42 @@ for {set j 1} {$j < $v} {incr j} {
     # network 1
     $ns duplex-link $n1(0) $n1($j) 1Mb 15ms DropTail
 
-    # set tcpa($j) [new Agent/TCP]
-    # $tcpa($j) set class_ 2
-    # $ns attach-agent $n1($j) $tcpa($j)
+    set tcpa($j) [new Agent/TCP]
+    $tcpa($j) set class_ 2
+    $ns attach-agent $n1($j) $tcpa($j)
 
-    # set sinka($j) [new Agent/TCPSink]
-    # $ns attach-agent $n1(0) $sinka($j)
-    # $ns connect $tcpa($j) $sinka($j)
-    # $tcpa($j) set fid_ 2
+    set sinka($j) [new Agent/TCPSink]
+    $ns attach-agent $n1(0) $sinka($j)
+    $ns connect $tcpa($j) $sinka($j)
+    $tcpa($j) set fid_ 2
 
-    # set ftpa($j) [new Application/FTP]
-    # $ftpa($j) attach-agent $tcpa($j)
-    # $ftpa($j) set type_ FTP
+    set ftpa($j) [new Application/FTP]
+    $ftpa($j) attach-agent $tcpa($j)
+    $ftpa($j) set type_ FTP
 
-    # $ns at 0.0 "$ftpa($j) start"
-    # $ns at $t "$ftpa($j) stop"
+    $ns at 0.0 "$ftpa($j) start"
+    $ns at $t "$ftpa($j) stop"
 
 
 
-    # set udpa($j) [new Agent/UDP]
-    # $ns attach-agent $n1(0) $udpa($j)
+    set udpa($j) [new Agent/UDP]
+    $ns attach-agent $n1(0) $udpa($j)
 
-    # set nulla($j) [new Agent/Null]
-    # $ns attach-agent $n1($j) $nulla($j)
+    set nulla($j) [new Agent/Null]
+    $ns attach-agent $n1($j) $nulla($j)
 
-    # $ns connect $udpa($j) $nulla($j)
-    # $udpa($j) set fid_ 1
+    $ns connect $udpa($j) $nulla($j)
+    $udpa($j) set fid_ 1
 
-    # set cbra($j) [new Application/Traffic/CBR]
-    # $cbra($j) attach-agent $udpa($j)
-    # $cbra($j) set type_ CBR
-    # $cbra($j) set packet_size_ 1000
-    # $cbra($j) set rate_ 1mb
-    # $cbra($j) set random_ true
+    set cbra($j) [new Application/Traffic/CBR]
+    $cbra($j) attach-agent $udpa($j)
+    $cbra($j) set type_ CBR
+    $cbra($j) set packet_size_ 1000
+    $cbra($j) set rate_ 1mb
+    $cbra($j) set random_ true
 
-    # $ns at 0.0 "$cbra($j) start"
-    # $ns at $t "$cbra($j) stop"
+    $ns at 0.0 "$cbra($j) start"
+    $ns at $t "$cbra($j) stop"
 
 
 }
---
+++ CW/Q2/top2.tcl
@@ -0,0 +1,98 @@
+set ns [new Simulator]
+
+$ns color 1 red
+$ns color 2 blue
+
+set nf [open top2.nam w]
+set tf [open top2.r w]
+
+$ns namtrace-all $nf
+$ns trace-all $tf
+
+
+
+$ns rtproto LS
+
+proc finish {} {
+    global ns nf
+
+    $ns flush-trace
+
+    close $nf
+
+    exec nam top1.nam &
+
+    exit 0
+}
+
+
+# seeting UDP communication
+
+
+set v 4
+set t 1.0
+
+for {set i 0} {$i < $v} {incr i} {
+    set n1($i) [$ns node]
+    # set n2($i) [$ns node]
+    # set n3($i) [$ns node]
+}
+
+
+for {set j 1} {$j < $v} {incr j} {
+
+    if { 1 == 1 } {
+        for {set k 0} {$k < $v} {incr} {
+            if { $k != $j} {
+
+                $ns duplex-link $n1(k) $n1($j) 1Mb 15ms DropTail
+
+                set tcpa($j) [new Agent/TCP]
+                $tcpa($j) set class_ 2
+                $ns attach-agent $n1($j) $tcpa($j)
+
+                set sinka($j) [new Agent/TCPSink]
+                $ns attach-agent $n1(k) $sinka($j)
+                $ns connect $tcpa($j) $sinka($j)
+                $tcpa($j) set fid_ 2
+
+                set ftpa($j) [new Application/FTP]
+                $ftpa($j) attach-agent $tcpa($j)
+                $ftpa($j) set type_ FTP
+
+                $ns at 0.0 "$ftpa($j) start"
+                $ns at $t "$ftpa($j) stop"
+
+
+
+                set udpa($j) [new Agent/UDP]
+                $ns attach-agent $n1(k) $udpa($j)
+
+                set nulla($j) [new Agent/Null]
+                $ns attach-agent $n1($j) $nulla($j)
+
+                $ns connect $udpa($j) $nulla($j)
+                $udpa($j) set fid_ 1
+
+                set cbra($j) [new Application/Traffic/CBR]
+                $cbra($j) attach-agent $udpa($j)
+                $cbra($j) set type_ CBR
+                $cbra($j) set packet_size_ 1000
+                $cbra($j) set rate_ 1mb
+                $cbra($j) set random_ true
+
+                $ns at 0.0 "$cbra($j) start"
+                $ns at $t "$cbra($j) stop"
+            }
+        }
+    }
+
+
+
+}
+
+
+
+$ns at 10.0 "finish"
+
+$ns run
