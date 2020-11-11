8933d5480dad538f7d3eb580e35e29f74deb1c15
xecarlox94 <jf94.uk@gmail.com>
2020-11-11 19:58:05 +0000

    workng on topology 1

--- CW/Q2/top1.tcl
+++ CW/Q2/top1.tcl
@@ -11,7 +11,7 @@ $ns trace-all $tf
 
 
 
-$ns rtproto DV
+$ns rtproto LS
 
 proc finish {} {
     global ns nf
@@ -29,15 +29,15 @@ proc finish {} {
 # seeting UDP communication
 
 
-set v 50
+set v 4
 set t 1.0
 
 
 
 for {set i 0} {$i < $v} {incr i} {
     set n1($i) [$ns node]
-    set n2($i) [$ns node]
-    set n3($i) [$ns node]
+    # set n2($i) [$ns node]
+    # set n3($i) [$ns node]
 }
 
 for {set j 1} {$j < $v} {incr j} {
@@ -45,127 +45,100 @@ for {set j 1} {$j < $v} {incr j} {
     # network 1
     $ns duplex-link $n1(0) $n1($j) 1Mb 15ms DropTail
 
-    set tcpa($j) [new Agent/TCP]
-    $tcpa($j) set class_ 2
-    $ns attach-agent $n1($j) $tcpa($j)
+    # set tcpa($j) [new Agent/TCP]
+    # $tcpa($j) set class_ 2
+    # $ns attach-agent $n1($j) $tcpa($j)
 
-    set sinka($j) [new Agent/TCPSink]
-    $ns attach-agent $n1(0) $sinka($j)
-    $ns connect $tcpa($j) $sinka($j)
-    $tcpa($j) set fid_ 2
+    # set sinka($j) [new Agent/TCPSink]
+    # $ns attach-agent $n1(0) $sinka($j)
+    # $ns connect $tcpa($j) $sinka($j)
+    # $tcpa($j) set fid_ 2
 
-    set ftpa($j) [new Application/FTP]
-    $ftpa($j) attach-agent $tcpa($j)
-    $ftpa($j) set type_ FTP
+    # set ftpa($j) [new Application/FTP]
+    # $ftpa($j) attach-agent $tcpa($j)
+    # $ftpa($j) set type_ FTP
 
-    $ns at 0.0 "$ftpa($j) start"
-    $ns at $t "$ftpa($j) stop"
-
-
-
-    set udpa($j) [new Agent/UDP]
-    $ns attach-agent $n1(0) $udpa($j)
-
-    set nulla($j) [new Agent/Null]
-    $ns attach-agent $n1($j) $nulla($j)
-
-    $ns connect $udpa($j) $nulla($j)
-    $udpa($j) set fid_ 1
-
-    set cbra($j) [new Application/Traffic/CBR]
-    $cbra($j) attach-agent $udpa($j)
-    $cbra($j) set type_ CBR
-    $cbra($j) set packet_size_ 1000
-    $cbra($j) set rate_ 1mb
-    $cbra($j) set random_ true
-
-    $ns at 0.0 "$cbra($j) start"
-    $ns at $t "$cbra($j) stop"
-
-
-    # network 2
-    $ns duplex-link $n2(0) $n2($j) 1Mb 15ms DropTail
-
-    set tcpb($j) [new Agent/TCP]
-    $tcpb($j) set class_ 2
-    $ns attach-agent $n2($j) $tcpb($j)
-
-    set sinkb($j) [new Agent/TCPSink]
-    $ns attach-agent $n2(0) $sinkb($j)
-    $ns connect $tcpb($j) $sinkb($j)
-    $tcpb($j) set fid_ 2
-
-    set ftpb($j) [new Application/FTP]
-    $ftpb($j) attach-agent $tcpb($j)
-    $ftpb($j) set type_ FTP
-
-    $ns at 0.0 "$ftpb($j) start"
-    $ns at $t "$ftpb($j) stop"
+    # $ns at 0.0 "$ftpa($j) start"
+    # $ns at $t "$ftpa($j) stop"
 
 
 
-    set udpb($j) [new Agent/UDP]
-    $ns attach-agent $n2(0) $udpb($j)
+    # set udpa($j) [new Agent/UDP]
+    # $ns attach-agent $n1(0) $udpa($j)
 
-    set nullb($j) [new Agent/Null]
-    $ns attach-agent $n2($j) $nullb($j)
+    # set nulla($j) [new Agent/Null]
+    # $ns attach-agent $n1($j) $nulla($j)
 
-    $ns connect $udpb($j) $nullb($j)
-    $udpb($j) set fid_ 1
+    # $ns connect $udpa($j) $nulla($j)
+    # $udpa($j) set fid_ 1
 
-    set cbrb($j) [new Application/Traffic/CBR]
-    $cbrb($j) attach-agent $udpb($j)
-    $cbrb($j) set type_ CBR
-    $cbrb($j) set packet_size_ 1000
-    $cbrb($j) set rate_ 1mb
-    $cbrb($j) set random_ true
+    # set cbra($j) [new Application/Traffic/CBR]
+    # $cbra($j) attach-agent $udpa($j)
+    # $cbra($j) set type_ CBR
+    # $cbra($j) set packet_size_ 1000
+    # $cbra($j) set rate_ 1mb
+    # $cbra($j) set random_ true
 
-    $ns at 0.0 "$cbrb($j) start"
-    $ns at $t "$cbrb($j) stop"
-
-    # network 3
+    # $ns at 0.0 "$cbra($j) start"
+    # $ns at $t "$cbra($j) stop"
 
 
-    $ns duplex-link $n3(0) $n3($j) 1Mb 15ms DropTail
-
-    set tcpc($j) [new Agent/TCP]
-    $tcpc($j) set class_ 2
-    $ns attach-agent $n3($j) $tcpc($j)
-
-    set sinkc($j) [new Agent/TCPSink]
-    $ns attach-agent $n3(0) $sinkc($j)
-    $ns connect $tcpc($j) $sinkc($j)
-    $tcpc($j) set fid_ 2
-
-    set ftpc($j) [new Application/FTP]
-    $ftpc($j) attach-agent $tcpc($j)
-    $ftpc($j) set type_ FTP
-
-    $ns at 0.0 "$ftpc($j) start"
-    $ns at $t "$ftpc($j) stop"
-
-
-
-    set udpc($j) [new Agent/UDP]
-    $ns attach-agent $n3(0) $udpc($j)
-
-    set nullc($j) [new Agent/Null]
-    $ns attach-agent $n3($j) $nullc($j)
-
-    $ns connect $udpc($j) $nullc($j)
-    $udpc($j) set fid_ 1
-
-    set cbrc($j) [new Application/Traffic/CBR]
-    $cbrc($j) attach-agent $udpc($j)
-    $cbrc($j) set type_ CBR
-    $cbrc($j) set packet_size_ 1000
-    $cbrc($j) set rate_ 1mb
-    $cbrc($j) set random_ true
-
-    $ns at 0.0 "$cbrc($j) start"
-    $ns at $t "$cbrc($j) stop"
 }
 
+
+set router [$ns node]
+# network 1
+$ns duplex-link $n1(0) $router 1Mb 15ms DropTail
+# $ns duplex-link $n1(2) $router 1Mb 15ms DropTail
+
+
+
+# setup a tcp agent
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+
+# set node as a TCP sender
+$ns attach-agent $router $tcp
+
+# set TCP receiver agent
+set sink [new Agent/TCPSink]
+
+# set node as TCP receiver
+$ns attach-agent $n1(3) $sink
+$ns connect $tcp $sink
+$tcp set fid_ 2
+
+
+# setup FTP application over TCP
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ftp set type_ FTP
+
+$ns at 0.0 "$ftp start"
+$ns at $t "$ftp stop"
+
+
+
+
+set udp [new Agent/UDP]
+$ns attach-agent $n1(1) $udp
+
+set null [new Agent/Null]
+$ns attach-agent $router $null
+
+
+$ns connect $udp $null
+$udp set fid_ 1
+
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ 1mb
+$cbr set random_ true
+
+$ns at 0.0 "$cbr start"
+$ns at $t "$cbr stop"
 
 
 $ns at 10.0 "finish"
