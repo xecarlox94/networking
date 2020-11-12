083a96b121cf085f0b27feec20c6882e776f9964
xecarlox94 <jf94.uk@gmail.com>
2020-11-12 22:52:42 +0000

    adding 3 networks to topology 1

--- CW/Q2/top1.tcl
+++ CW/Q2/top1.tcl
@@ -29,7 +29,7 @@ proc finish {} {
 # seeting UDP communication
 
 
-set v 20
+set v 3
 set t 1.0
 
 
@@ -43,7 +43,7 @@ for {set i 0} {$i < $v} {incr i} {
 for {set j 1} {$j < $v} {incr j} {
 
     # network 1
-    $ns duplex-link $n1(0) $n1($j) 1Mb 1ms DropTail
+    $ns duplex-link $n1(0) $n1($j) 1Mb 1ms RED
 
     set tcpa($j) [new Agent/TCP]
     $tcpa($j) set class_ 2
@@ -84,7 +84,7 @@ for {set j 1} {$j < $v} {incr j} {
 
 
     # network 2
-    $ns duplex-link $n2(0) $n2($j) 1Mb 15ms DropTail
+    $ns duplex-link $n2(0) $n2($j) 1Mb 15ms RED
 
     set tcpb($j) [new Agent/TCP]
     $tcpb($j) set class_ 2
@@ -123,8 +123,8 @@ for {set j 1} {$j < $v} {incr j} {
     $ns at 0.0 "$cbrb($j) start"
     $ns at $t "$cbrb($j) stop"
 
-    # network 1
-    $ns duplex-link $n3(0) $n3($j) 1Mb 15ms DropTail
+    # network 3
+    $ns duplex-link $n3(0) $n3($j) 1Mb 15ms RED
 
     set tcpc($j) [new Agent/TCP]
     $tcpc($j) set class_ 2
@@ -166,6 +166,96 @@ for {set j 1} {$j < $v} {incr j} {
 
 }
 
+set server [$ns node]
+
+$ns duplex-link $server $n1(0) 1Mb 10ms RED
+$ns duplex-link-op $server $n1(0) orient right
+
+set tcpr1 [new Agent/TCP]
+$tcpr1 set class_ 2
+$ns attach-agent $n1(0) $tcpr1
+
+set sinkr1 [new Agent/TCPSink]
+$ns attach-agent $server $sinkr1
+$ns connect $tcpr1 $sinkr1
+$tcpr1 set fid_ 2
+
+set ftpr1 [new Application/FTP]
+$ftpr1 attach-agent $tcpr1
+$ftpr1 set type_ FTP
+
+$ns at 0.0 "$ftpr1 start"
+$ns at $t "$ftpr1 stop"
+
+
+
+set udpr1 [new Agent/UDP]
+$ns attach-agent $server $udpr1
+
+set nullr1 [new Agent/Null]
+$ns attach-agent $n1(0) $nullr1
+$ns connect $udpr1 $nullr1
+$udpr1 set fid_ 1
+
+set cbrr1 [new Application/Traffic/CBR]
+$cbrr1 attach-agent $udpr1
+$cbrr1 set type_ CBR
+$cbrr1 set packet_size_ 1000
+$cbrr1 set rate_ 1mb
+$cbrr1 set random_ true
+
+$ns at 0.0 "$cbrr1 start"
+$ns at $t "$cbrr1 stop"
+
+
+
+
+$ns duplex-link $server $n2(0) 1Mb 10ms RED
+$ns duplex-link-op $server $n2(0) orient left
+
+
+set tcpr2 [new Agent/TCP]
+$tcpr2 set class_ 2
+$ns attach-agent $n2(0) $tcpr2
+
+set sinkr2 [new Agent/TCPSink]
+$ns attach-agent $server $sinkr2
+$ns connect $tcpr2 $sinkr2
+$tcpr2 set fid_ 2
+
+set ftpr2 [new Application/FTP]
+$ftpr2 attach-agent $tcpr2
+$ftpr2 set type_ FTP
+
+$ns at 0.0 "$ftpr2 start"
+$ns at $t "$ftpr2 stop"
+
+
+
+set udpr2 [new Agent/UDP]
+$ns attach-agent $server $udpr2
+
+set nullr2 [new Agent/Null]
+$ns attach-agent $n2(0) $nullr2
+$ns connect $udpr2 $nullr2
+$udpr2 set fid_ 1
+
+set cbrr2 [new Application/Traffic/CBR]
+$cbrr2 attach-agent $udpr2
+$cbrr2 set type_ CBR
+$cbrr2 set packet_size_ 1000
+$cbrr2 set rate_ 1mb
+$cbrr2 set random_ true
+
+$ns at 0.0 "$cbrr2 start"
+$ns at $t "$cbrr2 stop"
+
+
+
+
+
+# $ns duplex-link $server $n3(0) 1Mb 10ms RED
+# $ns duplex-link-op $server $n3(0) orient up
 
 
 $ns at $t "finish"
